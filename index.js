f0fda5219794a7ef8cca50438d63df15dad4e400
Daniele Ricci <daniele.ricci@buongiorno.com>
2015-11-02 22:37:07 +0100

    code complexity fix

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var child_process = require("child_process");
 var EventEmitter  = require("events");
 var fs            = require("fs");
 var path          = require("path");
+var utils         = require("./utils");
 
 function DaemonControl(daemon, filename, options) {
 	if(! (this instanceof DaemonControl))
@@ -23,30 +24,13 @@ DaemonControl.prototype = new EventEmitter();
 
 module.exports = DaemonControl;
 
-var commands = {
-	help:     null,
-	nodaemon: null,
-	reload:   null,
-	restart:  null,
-	start:    null,
-	status:   null,
-	stop:     null,
-};
+var commands = utils.commands;
+var hooks    = utils.hooks;
 
-var hooks = {
-	argv:     null,
-	help:     null,
-	kill:     null,
-	reload:   null,
-	running:  null,
-	start:    null,
-	starting: null,
-	status:   null,
-	stop:     null,
-	syntax:   null,
-	term:     null,
-	wait:     null,
-};
+DaemonControl.prototype._init         = utils._init;
+DaemonControl.prototype._init_options = utils._init_options;
+DaemonControl.prototype._init_spawn   = utils._init_spawn;
+DaemonControl.prototype._write        = utils._write;
 
 DaemonControl.prototype._cmdline = function(callback) {
 	if(process.argv.length < 3)
@@ -85,96 +69,6 @@ DaemonControl.prototype._help = function() {
 		return this.hooks.help(done);
 
 	done(true);
-};
-
-DaemonControl.prototype._init = function() {
-	if(! this.daemon)
-		throw new Error("DaemonControl: missing daemon parameter");
-
-	if("function" != typeof this.daemon)
-		throw new Error("DaemonControl: daemon parameter is not a function");
-
-	if(! this.filename)
-		throw new Error("DaemonControl: missing filename parameter");
-
-	if("string" != typeof this.filename)
-		throw new Error("DaemonControl: filename parameter is not a string");
-
-	if("undefined" == typeof this.options)
-		this.options = {};
-	else
-		if("object" != typeof this.options)
-			throw new Error("DaemonControl: options parameter is not an object");
-
-	this._init_options();
-};
-
-DaemonControl.prototype._init_options = function() {
-	var i;
-
-	if(! this.options.hooks)
-		this.hooks = {};
-	else {
-		this.hooks = this.options.hooks;
-		delete this.options.hooks;
-
-		if("object" != typeof this.hooks)
-			throw new Error("DaemonControl: options.hooks is not an object");
-	}
-
-	for(i in this.hooks) {
-		if(! (i in hooks))
-			throw new Error("DaemonControl: unknow hook options.hooks." + i);
-
-		if("function" != typeof this.hooks[i])
-			throw new Error("DaemonControl: options.hooks." + i + " is not a function");
-	}
-
-	if("reload" in this.options) {
-		this.reload = this.options.reload;
-		delete this.options.reload;
-	}
-
-	if("timeout" in this.options) {
-		this.timeout = parseInt(this.options.timeout);
-		delete this.options.timeout;
-
-		if(isNaN(this.timeout))
-			throw new Error("DaemonControl: timeout parameter is not an integer number");
-
-		if(this.timeout <= 0)
-			throw new Error("DaemonControl: timeout parameter is not a non zero positive integer number");
-	}
-	else
-		this.timeout = 5;
-
-	this._init_spawn();
-};
-
-DaemonControl.prototype._init_spawn = function() {
-	var i;
-
-	if(! ("cwd" in this.options))
-		this.options.cwd = process.cwd();
-	else
-		if("string" != typeof this.options.cwd)
-			throw new Error("DaemonControl: cwd option is not a string");
-
-	if(! ("env" in this.options)) {
-		this.options.env = {};
-
-		for(i in process.env)
-			this.options.env[i] = process.env[i];
-	}
-	else
-		if("object" != typeof this.options.env)
-			throw new Error("DaemonControl: env option is not an object");
-
-	if(! ("stdio" in this.options))
-		this.options.stdio = "ignore";
-
-	if(! ("detached" in this.options))
-		this.options.detached = true;
 };
 
 DaemonControl.prototype._kill = function(pid, callback) {
@@ -448,8 +342,4 @@ DaemonControl.prototype._wait = function(pid, callback, killed, count) {
 
 		done(true);
 	}, 1000);
-};
-
-DaemonControl.prototype._write = function(msg) {
-	process.stdout.write(msg);
 };
---
+++ utils.js
@@ -0,0 +1,129 @@
+"use strict";
+
+var commands = {
+	help:     null,
+	nodaemon: null,
+	reload:   null,
+	restart:  null,
+	start:    null,
+	status:   null,
+	stop:     null,
+};
+
+var hooks = {
+	argv:     null,
+	help:     null,
+	kill:     null,
+	reload:   null,
+	running:  null,
+	start:    null,
+	starting: null,
+	status:   null,
+	stop:     null,
+	syntax:   null,
+	term:     null,
+	wait:     null,
+};
+
+function _init() {
+	if(! this.daemon)
+		throw new Error("DaemonControl: missing daemon parameter");
+
+	if("function" != typeof this.daemon)
+		throw new Error("DaemonControl: daemon parameter is not a function");
+
+	if(! this.filename)
+		throw new Error("DaemonControl: missing filename parameter");
+
+	if("string" != typeof this.filename)
+		throw new Error("DaemonControl: filename parameter is not a string");
+
+	if("undefined" == typeof this.options)
+		this.options = {};
+	else
+		if("object" != typeof this.options)
+			throw new Error("DaemonControl: options parameter is not an object");
+
+	this._init_options();
+}
+
+function _init_options() {
+	var i;
+
+	if(! this.options.hooks)
+		this.hooks = {};
+	else {
+		this.hooks = this.options.hooks;
+		delete this.options.hooks;
+
+		if("object" != typeof this.hooks)
+			throw new Error("DaemonControl: options.hooks is not an object");
+	}
+
+	for(i in this.hooks) {
+		if(! (i in hooks))
+			throw new Error("DaemonControl: unknow hook options.hooks." + i);
+
+		if("function" != typeof this.hooks[i])
+			throw new Error("DaemonControl: options.hooks." + i + " is not a function");
+	}
+
+	if("reload" in this.options) {
+		this.reload = this.options.reload;
+		delete this.options.reload;
+	}
+
+	if("timeout" in this.options) {
+		this.timeout = parseInt(this.options.timeout);
+		delete this.options.timeout;
+
+		if(isNaN(this.timeout))
+			throw new Error("DaemonControl: timeout parameter is not an integer number");
+
+		if(this.timeout <= 0)
+			throw new Error("DaemonControl: timeout parameter is not a non zero positive integer number");
+	}
+	else
+		this.timeout = 5;
+
+	this._init_spawn();
+}
+
+function _init_spawn() {
+	var i;
+
+	if(! ("cwd" in this.options))
+		this.options.cwd = process.cwd();
+	else
+		if("string" != typeof this.options.cwd)
+			throw new Error("DaemonControl: cwd option is not a string");
+
+	if(! ("env" in this.options)) {
+		this.options.env = {};
+
+		for(i in process.env)
+			this.options.env[i] = process.env[i];
+	}
+	else
+		if("object" != typeof this.options.env)
+			throw new Error("DaemonControl: env option is not an object");
+
+	if(! ("stdio" in this.options))
+		this.options.stdio = "ignore";
+
+	if(! ("detached" in this.options))
+		this.options.detached = true;
+}
+
+function _write(msg) {
+	process.stdout.write(msg);
+}
+
+module.exports = {
+	_init:         _init,
+	_init_options: _init_options,
+	_init_spawn:   _init_spawn,
+	_write:        _write,
+	commands:      commands,
+	hooks:         hooks,
+};
